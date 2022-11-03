b402647bcf241002c289550d0630b780b25a59b0
leeheezin <gmlwls107@naver.com>
2022-11-03 15:37:42 +0900

    컴포넌트 생성

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <header>
+        <div class="header_inner">
+            <div class="logo">LK PARTNERS</div>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">ABOUT</a>
+                        <ul class="subNav">
+                            <li><a href="#">LK PARTNERS INC.</a></li>
+                            <li><a href="#">HISTORY</a></li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#">BUSINESS</a>
+                        <ul class="subNav">
+                            <li><a href="#">MEFLEX</a></li>
+                            <li><a href="#">ENTERTAINMENT</a></li>
+                            <li><a href="#">AD/MARKETING</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">CREATOR</a></li>
+                    <li><a href="#">NEWS</a></li>
+                    <li><a href="#">CONTACT</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+<script>
+
+</script>
+<style lang="scss" scoped>
+</style>
