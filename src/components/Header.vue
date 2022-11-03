35ed59f8a66ecc1764113d2d922e65a424e0d31e
leeheezin <gmlwls107@naver.com>
2022-11-03 17:55:54 +0900

    header작업

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,19 +1,27 @@
 <template>
     <header>
-        <div class="header_inner">
+        <div class="header_inner"
+            @mouseleave="[( gnb1= false ), (gnb2=false)]"
+        >
             <div class="logo">LK PARTNERS</div>
             <nav>
-                <ul>
+                <ul class="nav">
                     <li>
-                        <a href="#">ABOUT</a>
-                        <ul class="subNav">
+                        <a href="#"
+                            @mouseover="( gnb1 = true)"
+                            @click = "( gnb1 = true )"
+                        >ABOUT</a>
+                        <ul class="subNav" v-show="gnb1">
                             <li><a href="#">LK PARTNERS INC.</a></li>
                             <li><a href="#">HISTORY</a></li>
                         </ul>
                     </li>
                     <li>
-                        <a href="#">BUSINESS</a>
-                        <ul class="subNav">
+                        <a href="#"
+                            @mouseover="( gnb2 = true)"
+                            @click = "( gnb2 = true )"
+                        >BUSINESS</a>
+                        <ul class="subNav" v-show="gnb2">
                             <li><a href="#">MEFLEX</a></li>
                             <li><a href="#">ENTERTAINMENT</a></li>
                             <li><a href="#">AD/MARKETING</a></li>
@@ -28,7 +36,14 @@
     </header>
 </template>
 <script>
-
+export default {
+    data: () => ({
+        gnb1: false,
+        gnb2: false,
+    })
+}
 </script>
 <style lang="scss" scoped>
+@import "../assets/css/header.scss";
+@import "../assets/css/reset.scss";
 </style>
